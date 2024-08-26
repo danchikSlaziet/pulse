7c2efe4c5d87a6736f29e322e9c98653bae363b7
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2024-08-26 22:38:12 +0300

    dfdfdf

---
+++ index.js
@@ -0,0 +1,85 @@
+function parseQuery(queryString) {
+  let query = {};
+  let pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+  for (let i = 0; i < pairs.length; i++) {
+      let pair = pairs[i].split('=');
+      query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+  }
+  return query;
+}
+let userChatId;
+document.addEventListener('DOMContentLoaded', () => {
+  let app = window.Telegram.WebApp;
+  let query = app.initData;
+  let user_data_str = parseQuery(query).user;
+  let user_data = JSON.parse(user_data_str);
+  app.disableVerticalSwipes();
+  app.expand();
+  app.ready();
+  userChatId = user_data["id"];
+});
+function vibro() {
+  let detect = new MobileDetect(window.navigator.userAgent);
+  if (detect.os() === 'iOS') {
+    window.Telegram.WebApp.HapticFeedback.impactOccurred("light");
+  }
+  else {
+    if ("vibrate" in navigator) {
+      // Вибрируем устройство в течение 1000 миллисекунд (1 секунда)
+      navigator.vibrate(10);
+    } else {
+      // Ваш браузер не поддерживает API вибрации
+      console.log("Ваш браузер не поддерживает API вибрации.");
+    }
+  }
+}
+
+const pulseButton = document.getElementById('pulseButton');
+let pulseInterval;
+let isPulseActive = false;
+
+// Определяем ритм пульса в виде массива интервалов между ударами
+const pulseRhythm = [600, 800, 700, 600, 750, 850];
+
+function startPulse() {
+  let currentIndex = 0;
+
+  pulseInterval = setInterval(() => {
+    // if (navigator.vibrate) {
+    //   navigator.vibrate(50);
+    // }
+    vibro();
+
+    currentIndex++;
+    if (currentIndex >= pulseRhythm.length) {
+      currentIndex = 0;
+    }
+
+    const nextPulseDelay = pulseRhythm[currentIndex];
+    setTimeout(() => {
+      if (isPulseActive) {
+        startPulse();
+      }
+    }, nextPulseDelay);
+  }, 50);
+
+  pulseButton.textContent = 'Остановочка пульса';
+  isPulseActive = true;
+}
+
+function stopPulse() {
+  clearInterval(pulseInterval);
+  if (navigator.vibrate) {
+    navigator.vibrate(0);
+  }
+  pulseButton.textContent = 'Пульс';
+  isPulseActive = false;
+}
+
+pulseButton.addEventListener('click', () => {
+  if (!isPulseActive) {
+    startPulse();
+  } else {
+    stopPulse();
+  }
+});
